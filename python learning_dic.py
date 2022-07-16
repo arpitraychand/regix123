f04949a282a02a01256c53b617c601ff3ce83220
arpitraychand <108273355+arpitraychand@users.noreply.github.com>
2022-07-16 13:28:39 +0530

    commit

---
+++ python learning_dic.py
@@ -0,0 +1,18 @@
+
+# map_= {
+
+#     'first_name': 'Arpit',
+#     'last_name': 'Raychand Sansi',
+#     'company' : 'Software'
+# }
+# print(map_['first_name'])
+# print(map_['last_name'])
+# # print(map_['company'])
+# print(map_.get('company'))
+# print(map_.get('company','Not Found'))
+# map_.update({'company':'starks'})
+# print(map_)
+# print(map_['company'])
+# company = map_.pop('company')
+# print('company')
+# print(map_)
---
+++ python_string_method.py
@@ -0,0 +1,49 @@
+
+import fileinput
+import random
+from re import A
+from turtle import st
+
+
+# first_name = 'arpit'
+# last_name = 'raychand'
+# sentence = f'{first_name} {last_name} is a coder '
+# print(sentence)
+
+
+# lst = [1,2,3,4,5,'Hello',['a','b','c']]
+# print(lst[0])
+# print([-1])
+# print([-2][0])
+# lst = [1,2,3,4,5,'hello',['a','b']]
+# random_number_list = [10,33,40,22,76,90,23,87]
+# random_number_list.sort()
+# print(random_number_list)
+# print(random_number_list,'before appending anything.')
+# random_number_list.append("hello")
+# print(random_number_list,'after appending anything.')
+
+# ls = 'arpit'
+# s = list(ls)
+# print(s)
+# s.sort()
+# print(s)
+# a = [1,3,5]
+# b = [6,8,9,]
+# print(a + b)
+# a.append(b)
+# print(a)
+# a.extend(b)
+# print(a)    
+# st = set([1,2,3,4,5])
+# print(st)
+# st = {4,5,6,6,3,3,1}
+# print(st)
+
+# bt = {4,5,6}
+# print(type(st))
+
+# print(st.union(bt))
+# print(st.intersection(bt))
+# print(st.difference(bt))
+
